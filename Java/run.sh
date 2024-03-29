a5ddf4f4ecdb757730cc9d8e33d36b72a3a454fa
Ruan C. Keet <26340461@sun.ac.za>
2024-03-29 16:40:43 +0200

    Removed Java/Python versions

--- Java/run.sh
+++
@@ -1,6 +0,0 @@
-
-echo "compiling..."
-javac -d bin src/*.java
-
-echo "running $1"
-time java -cp bin BFVM $1
--- Java/src/ByteCode.java
+++
@@ -1,9 +0,0 @@
-public class ByteCode {
-    public OpCode code;
-    public int operand;
-
-    public ByteCode(OpCode code, int operand) {
-        this.code = code;
-        this.operand = operand;
-    }
-}
