8a31897502ac4068a12344d5f70c9084c9b514aa
Ruan C. Keet <26340461@sun.ac.za>
2024-03-29 17:19:59 +0200

    Removed a lot of bloat from the error module

--- C/src/error.c
+++ C/src/error.c
@@ -15,63 +15,11 @@
 
 /* --- ASCII colors ---------------------------------------------------------*/
 
-#define ESC                      "\033["
-#define BOLD                     "1;"
-#define ASCII_RESET              ESC "m"
+#define ASCII_BOLD_CYAN "\033[1;36m"
+#define ASCII_BOLD_WHITE "\033[1;37m"
+#define ASCII_BOLD_RED "\033[1;31m"
 
-#define BLACK                    "30m"
-#define RED                      "31m"
-#define GREEN                    "32m"
-#define YELLOW                   "33m"
-#define BLUE                     "34m"
-#define MAGENTA                  "35m"
-#define CYAN                     "36m"
-#define WHITE                    "37m"
-
-#define HIGH_BLACK               "90m"
-#define HIGH_RED                 "91m"
-#define HIGH_GREEN               "92m"
-#define HIGH_YELLOW              "93m"
-#define HIGH_BLUE                "94m"
-#define HIGH_MAGENTA             "95m"
-#define HIGH_CYAN                "96m"
-#define HIGH_WHITE               "97m"
-
-#define ASCII_BLACK              ESC BLACK
-#define ASCII_RED                ESC RED
-#define ASCII_GREEN              ESC GREEN
-#define ASCII_YELLOW             ESC YELLOW
-#define ASCII_BLUE               ESC BLUE
-#define ASCII_MAGENTA            ESC MAGENTA
-#define ASCII_CYAN               ESC CYAN
-#define ASCII_WHITE              ESC WHITE
-
-#define ASCII_BOLD_BLACK         ESC BOLD BLACK
-#define ASCII_BOLD_RED           ESC BOLD RED
-#define ASCII_BOLD_GREEN         ESC BOLD GREEN
-#define ASCII_BOLD_YELLOW        ESC BOLD YELLOW
-#define ASCII_BOLD_BLUE          ESC BOLD BLUE
-#define ASCII_BOLD_MAGENTA       ESC BOLD MAGENTA
-#define ASCII_BOLD_CYAN          ESC BOLD CYAN
-#define ASCII_BOLD_WHITE         ESC BOLD WHITE
-
-#define ASCII_HIGH_BLACK         ESC HIGH_BLACK
-#define ASCII_HIGH_RED           ESC HIGH_RED
-#define ASCII_HIGH_GREEN         ESC HIGH_GREEN
-#define ASCII_HIGH_YELLOW        ESC HIGH_YELLOW
-#define ASCII_HIGH_BLUE          ESC HIGH_BLUE
-#define ASCII_HIGH_MAGENTA       ESC HIGH_MAGENTA
-#define ASCII_HIGH_CYAN          ESC HIGH_CYAN
-#define ASCII_HIGH_WHITE         ESC HIGH_WHITE
-
-#define ASCII_BOLD_HIGH_BLACK    ESC BOLD HIGH_BLACK
-#define ASCII_BOLD_HIGH_RED      ESC BOLD HIGH_RED
-#define ASCII_BOLD_HIGH_GREEN    ESC BOLD HIGH_GREEN
-#define ASCII_BOLD_HIGH_YELLOW   ESC BOLD HIGH_YELLOW
-#define ASCII_BOLD_HIGH_BLUE     ESC BOLD HIGH_BLUE
-#define ASCII_BOLD_HIGH_MAGENTA  ESC BOLD HIGH_MAGENTA
-#define ASCII_BOLD_HIGH_CYAN     ESC BOLD HIGH_CYAN
-#define ASCII_BOLD_HIGH_WHITE    ESC BOLD HIGH_WHITE
+#define ASCII_RESET "\033[m"
 
 /* --- global variables -----------------------------------------------------*/
 
@@ -127,9 +75,8 @@ void log_norm(const char *fmt, ...)
 
 void log_info(const char *fmt, ...)
 {
-    int istty = isatty(2);
+    const char *prefix = ASCII_BOLD_CYAN "info:" ASCII_RESET;
     va_list args;
-    const char *prefix = istty ? ASCII_BOLD_CYAN "info:" ASCII_RESET : "info:";
 
     va_start(args, fmt);
     cprintf(stdout, prefix, fmt, args);
@@ -138,10 +85,8 @@ void log_info(const char *fmt, ...)
 
 void log_err(const char *fmt, ...)
 {
-    int istty = isatty(2);
+    const char *prefix = ASCII_BOLD_RED "error:" ASCII_RESET;
     va_list args;
-    const char *prefix = istty ? ASCII_BOLD_RED "error:" ASCII_RESET :
-        "error:";
 
     va_start(args, fmt);
     cprintf(stderr, prefix, fmt, args);
@@ -152,21 +97,20 @@ void log_err(const char *fmt, ...)
 
 void log_errpos(const char *fmt, ...)
 {
-    int istty = isatty(2);
+    #define MAX_PREFIX_SIZE 512
+
+    const char *err = ASCII_BOLD_RED "error:" ASCII_RESET;
     va_list args;
-    char prefix[512];
-    const char *start = istty ? ASCII_BOLD_WHITE : "";
-    const char *end = istty ? ASCII_RESET : "";
-    const char *err = istty ? ASCII_BOLD_RED "error:" ASCII_RESET : "error:";
+    char prefix[MAX_PREFIX_SIZE];
 
     if (progname) {
-        snprintf(prefix, 512, "%s%s%s:%s%lu:%lu%s: %s",
-            start, progname, end,
-            start, position.line, position.column, end,
+        snprintf(prefix, MAX_PREFIX_SIZE, "%s%s%s:%s%lu:%lu%s: %s",
+            ASCII_BOLD_WHITE, progname, ASCII_RESET,
+            ASCII_BOLD_WHITE, position.line, position.column, ASCII_RESET,
             err);
     } else {
-        snprintf(prefix, 512, "%s%lu:%lu%s: %s",
-            start, position.line, position.column, end,
+        snprintf(prefix, MAX_PREFIX_SIZE, "%s%lu:%lu%s: %s",
+            ASCII_BOLD_WHITE, position.line, position.column, ASCII_RESET,
             err);
     }
 
@@ -218,12 +162,8 @@ char *estrdup(const char *s)
 static void cprintf(FILE *stream, const char *prefix, const char *fmt,
     va_list args)
 {
-    int istty = isatty(2);
-    const char *ac_start = (istty ? ASCII_BOLD_WHITE : "");
-    const char *ac_end = (istty ? ASCII_RESET : "");
-
     fflush(stdout);
-    fprintf(stream, "%sbfvm:%s ", ac_start, ac_end);
+    fprintf(stream, "%sbfvm:%s ", ASCII_BOLD_WHITE, ASCII_RESET);
     if (prefix != NULL) {
         fprintf(stream, "%s ", prefix);
     }
